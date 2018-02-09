dbf38bc0f0799649347a9f5d78cc634d522b1ca3
Jerry <jerrytiapalmiotto@gmail.com>
2018-02-09 16:27:14 +0100

    Controllo di dati Input in PHP

--- php/signup.php
+++ php/signup.php
@@ -9,6 +9,10 @@ $nome = $_POST['nome'];
 $cognome = $_POST['cognome'];
 $email = $_POST['email'];
 
-$Connection->conn->query("INSERT INTO user_list (id, username, password, nome, cognome, email, tipo_utente) VALUES (null, '$username', '$password', '$nome', '$cognome', '$email', 2)");
+if ($username or $password or $nome or $cognome or $email == null):
+    header('location /');
+else:
+    $Connection->conn->query("INSERT INTO user_list (id, username, password, nome, cognome, email, tipo_utente) VALUES (null, '$username', '$password', '$nome', '$cognome', '$email', 2)");
+endif;
 
 header('location: /');
