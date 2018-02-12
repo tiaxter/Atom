96db475d37e145c58caeb2b10041d9f9b98a5aae
Jerry <jerrytiapalmiotto@gmail.com>
2018-02-12 22:14:50 +0100

    Aggiunta Modifica Account da Parte dell'Utente Amministratore

--- js/lista-utenti.js
+++ js/lista-utenti.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
-    alertify.genericDialog || alertify.dialog('genericDialog',function(){
+    alertify.genericDialog || alertify.dialog('genericDialog', function () {
         return {
-            main:function(content){
+            main: function (content) {
                 this.setContent(content);
             },
-            setup:function(){
+            setup: function () {
                 return {
-                    focus:{
-                        element:function(){
+                    focus: {
+                        element: function () {
                             return this.elements.body.querySelector(this.get('selector'));
                         },
-                        select:true
+                        select: true
                     },
-                    options:{
-                        basic:true,
-                        maximizable:false,
-                        resizable:false,
-                        padding:false
+                    options: {
+                        basic: true,
+                        maximizable: false,
+                        resizable: false,
+                        padding: false
                     }
                 };
             },
-            settings:{
-                selector:undefined
+            settings: {
+                selector: undefined
             }
         };
     });
@@ -30,7 +30,7 @@ $(document).ready(function () {
         $.ajax({
             url: '../static/edit-account.php',
             type: 'POST',
-            data:{id:id},
+            data: {id: id},
             success: function (data) {
                 $('#edit-account').html(data);
             }
@@ -38,14 +38,33 @@ $(document).ready(function () {
         alertify.genericDialog($('#edit-account')[0]);
     });
     $(document).on('click', '#edit-btn', function () {
-        $.ajax({
-           url: '../php/changesetting.php',
-           type: 'POST',
-           data:{id_n : $("input[name='ID']").val(), username : $("input[name='username']").val(), password : $("input[name='password']").val(), nome : $("input[name='nome']").val(), cognome:$("input[name='cognome']").val(), email:$("input[name='email']").val(), ruolo:$("select[name='ruolo']").val()},
-           success : function (data) {
-               alertify.closeAll();
-               alertify.set('notifier','position', 'top-center');
-           }
+        var error = 0;
+        $(".form-control:not(input[name='password'])").each(function () {
+            if ($(this).val() == '') {
+                error++;
+            }
         });
+        if (error > 0) {
+            alertify.alert('Atom', 'Completa tutti i campi');
+        } else {
+            $.ajax({
+                url: '../php/changesetting.php',
+                type: 'POST',
+                data: {
+                    id_n: $("input[name='ID']").val(),
+                    username: $("input[name='username']").val(),
+                    password: $("input[name='password']").val(),
+                    nome: $("input[name='nome']").val(),
+                    cognome: $("input[name='cognome']").val(),
+                    email: $("input[name='email']").val(),
+                    ruolo: $("select[name='ruolo']").val()
+                },
+                success: function (data) {
+                    alertify.closeAll();
+                    alertify.set('notifier', 'position', 'top-center');
+                    alertify.alert('Atom', 'Modifiche effettuate');
+                }
+            });
+        };
     });
 });
--- php/search_anime.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once ('db_manage.php');
-$Connection = new db_manage();
-if (isset($_GET['search'])):
-
-endif;
