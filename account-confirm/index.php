af9408104e52100516d4fe2e5c732a5300e946e7
Jerry <jerrytiapalmiotto@gmail.com>
2018-02-17 22:57:29 +0100

    Richiesta Attivazione Account da Mail

---
+++ account-confirm/index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <?php require('../static/head.php'); ?>
+</head>
+<body>
+<?php /*require ('../static/navbar.php');*/ ?>
+</body>
+<?php
+require_once('../php/db_manage.php');
+$Connection = new db_manage();
+$username = isset($_GET['username']) ? $_GET['username'] : null;
+$activation_code = isset($_GET['activation_code']) ? $_GET['activation_code'] : null;
+if ($activation_code == null || $username == null):
+    echo('Dati invalidi');
+else:
+    $acc_status = ($Connection->conn->query("SELECT active from user_list WHERE username='$username' AND activation_code='$activation_code'"))->fetch_assoc();
+    $acc_status = $acc_status['active'];
+    if ($acc_status == 1):
+        ?>
+        <script>
+            $(document).ready(function () {
+                alertify.alert('Atom', 'Account già attivato',function () {
+                    location.replace('/');
+                });
+            });
+        </script>
+    <?php
+    else:
+    $Connection->conn->query("UPDATE user_list SET active=1 WHERE username='$username' AND activation_code='$activation_code'");
+    $acc_status = ($Connection->conn->query("SELECT active from user_list WHERE username='$username' AND activation_code='$activation_code'"))->fetch_assoc();
+    $acc_status = $acc_status['active'];
+    if ($acc_status == 1):
+    ?>
+        <script>
+            $(document).ready(function () {
+                alertify.alert('Atom', 'Account attivato',function () {
+                    location.replace('/');
+                });
+            })
+        </script>
+    <?php
+    else:
+    ?>
+        <script>
+            $(document).ready(function () {
+                alertify.alert('Atom', 'Attivazione non riuscita',function () {
+                    location.replace('/');
+                });
+            })
+        </script>
+    <?php
+    endif;
+    endif;
+endif;
+?>
+</html>
--- php/signup.php
+++ php/signup.php
@@ -9,7 +9,7 @@ $password = isset($_POST['password']) ? hash('sha256', $_POST['password']) : nul
 $nome = isset($_POST['nome']) ? $_POST['nome'] : null;
 $cognome = isset($_POST['cognome']) ? $_POST['cognome'] : null;
 $email = isset($_POST['email']) ? $_POST['email'] : null;
-
+$activation_code = hash('sha256',rand());
 /*Invio mail*/
 
 $header = "From: atom.notreply@gmail.com <atom.notreply@gmail.com>\r\n".
@@ -19,9 +19,11 @@ $email_body = file_get_contents("../static/email.php");
 
 $email_body = str_replace("username",$username,$email_body);
 $email_body = str_replace("pswd",$tr_password,$email_body);
+$email_body = str_replace('verifica','http://www.atom.local/account-confirm/?username='.$username.'&activation_code='.$activation_code, $email_body);
 
 $mail = mail($email,'Benvenuto',$email_body,$header);
 
-/*$Connection->conn->query("INSERT INTO user_list (id, username, password, nome, cognome, email, tipo_utente) VALUES (null, '$username', '$password', '$nome', '$cognome', '$email', 2)");*/
+
+$Connection->conn->query("INSERT INTO user_list (id, username, password, nome, cognome, email, tipo_utente, active, activation_code) VALUES (null, '$username', '$password', '$nome', '$cognome', '$email', 2, 0, '$activation_code')");
 echo("Registrazione Effettuata");
 
