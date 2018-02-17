1b1a180558c190e662a0e8736193fbf9a560df6d
Jerry <jerrytiapalmiotto@gmail.com>
2018-02-17 02:40:52 +0100

    Invio Mail con HTML, finalmente

--- php/signup.php
+++ php/signup.php
@@ -10,9 +10,14 @@ $nome = isset($_POST['nome']) ? $_POST['nome'] : null;
 $cognome = isset($_POST['cognome']) ? $_POST['cognome'] : null;
 $email = isset($_POST['email']) ? $_POST['email'] : null;
 
-$msg = "Benvenuto su Atom ".$nome."\nTi sei registrato con i seguenti dati\nUsername: ".$username.", Password: ".$tr_password;
+/*Invio mail*/
 
-$mail = mail($email, 'Registrazione ad Atom', $msg, 'atom.notreply@gmail.com');
+$header = "From: atom.notreply@gmail.com <atom.notreply@gmail.com>\r\n".
+    "MIME-Version: 1.0\r\n" .
+    "Content-type: text/html; charset=UTF-8\r\n";
+$email_body = file_get_contents("../static/email.php");
+$mail = mail($email,'Benvenuto',$email_body,$header);
 
-$Connection->conn->query("INSERT INTO user_list (id, username, password, nome, cognome, email, tipo_utente) VALUES (null, '$username', '$password', '$nome', '$cognome', '$email', 2)");
+/*$Connection->conn->query("INSERT INTO user_list (id, username, password, nome, cognome, email, tipo_utente) VALUES (null, '$username', '$password', '$nome', '$cognome', '$email', 2)");*/
 echo("Registrazione Effettuata");
+
---
+++ static/email.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+</head>
+<body>
+    <h1>Benvenuto su Atom</h1>
+    <p>Ciao</p>
+</body>
+</html>
